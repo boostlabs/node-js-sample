92d43cb4276661fb6563ca4b9759d0bee6939cf4
jay compton <boost09@gmail.com>
2017-03-02 16:24:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('This is my first node.js application!')
+  response.send('This is my first node.js application')
 })
 
 app.listen(app.get('port'), function() {
